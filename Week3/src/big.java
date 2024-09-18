31862485be563aaf3843de0673640a5b96ec98b8
nickbakker68@gmail.com <nickbakker68@gmail.com>
2024-09-18 16:21:41 +0200

    huiswerk

--- Week3/src/big.java
+++ Week3/src/big.java
@@ -3,16 +3,41 @@ public class big {
 
     public static void main(String[] args) {
 
-
+        System.out.println(factorial(5));
+        System.out.println(factor(50));
 
     }
 
 
-    
+    public static int factorial(int n)
+    {
+        if(n == 1) {
+            return 1;
+        }
+            return n*factorial(n-1);
+
+    }
 
 
 
+public static int factor(int n)
+{
+    if(n == 1) {
+        return 1;
+    }
+    else
 
+    {
+        int a = 1;
+
+       for(int i = 2; i <= n; i++) {
+           a=a*i;
+           System.out.println(i + ":" + a);
+        }
+        return a;
+
+    }
+}
 
 
 
