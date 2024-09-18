4db0f84f80a23246165c6f36ad6a200aa1b2006d
nickbakker68@gmail.com <nickbakker68@gmail.com>
2024-09-18 11:23:42 +0200

    daystokerst_toegevoegd

--- Week3/src/dateandtime.java
+++ Week3/src/dateandtime.java
@@ -1,8 +1,10 @@
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 public class dateandtime
 {
+
+
 
     public static void main(String[] args)
     {
@@ -14,15 +16,69 @@ public class dateandtime
         LocalDate sinterklaas = LocalDate.of(2024, 12 ,05);
         System.out.println(sinterklaas.getDayOfWeek());
 
+
+
+        LocalDate datum = LocalDate.of(2024, 9, 18); // Stel een datum in
+        boolean Woensdag = isWoensdag(datum);
+
+        System.out.println("Is het woensdag? " + Woensdag);
+
+        int dagentotkersttt = aantalDagenTotKerst();
+        System.out.println(dagentotkersttt);
+
     }
 
-    public static boolean woensdagofniet(LocalDate datum){
-
-
-        LocalDate woensdag = LocalDate.of(datum);
-        
+    public static boolean isWoensdag(LocalDate datum)
+    {
+        // Controleer of de gegeven datum een woensdag is
+        return datum.getDayOfWeek() == DayOfWeek.WEDNESDAY;
     }
+
+    public static int aantalDagenTotKerst()
+    {
+
+        LocalDate kdag1 = LocalDate.of(LocalDate.now().getYear(), 12,25);
+        LocalDate kdag2 = LocalDate.of(LocalDate.now().getYear(), 12,26);
+        LocalDate vdaag = LocalDate.now();
+
+        if(vdaag.isEqual(kdag1) || vdaag.isEqual(kdag2))
+        {
+            return 0;
+
+        }
+        else
+            if(vdaag.isBefore(kdag1))
+            {
+                return (int) ChronoUnit.DAYS.between(vdaag, kdag1);
+
+            }
+            else
+            if(vdaag.isAfter(kdag1))
+            {
+
+                return (int) ChronoUnit.DAYS.between(vdaag, kdag1.plusYears(1));
+            }
+
+
+
+
+
+
+
+
+
+        return 0;
+
+
+    }
+
+
+
 
 
 
 }
+
+
+
+
